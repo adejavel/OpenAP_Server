19c98a617d080f4d51e53ad562a884e1161cc94e
adejavel <adejavel@cisco.com>
2018-11-01 12:16:25 +0100

    adding logging

--- openap_server/settings.py
+++ openap_server/settings.py
@@ -82,6 +82,25 @@ TEMPLATES = [
     },
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': './debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
+
 WSGI_APPLICATION = 'openap_server.wsgi.application'
 
 
