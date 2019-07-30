3ffa2570bf4c7730ec0acb949bb48018fcfbed05
adejavel <adejavel@cisco.com>
2019-07-30 16:31:41 +0200

    adding database flush

--- devices/management/commands/dropdata.py
+++ devices/management/commands/dropdata.py
@@ -13,27 +13,29 @@ class Command(BaseCommand):
         max_date = time.time()-7*24*60*60
         dvs = devices.find({})
         for device in dvs:
-            device_data = device["data"]
-            new_data = []
-            for d in device_data:
-                if d["timestamp"]>max_date:
-                    new_data.append(d)
-            devices.update_one({
-                'mac_address': device["mac_address"]
-            }, {"$set": {
-                "data": new_data,
-            }
-            }, upsert=False)
+            if "data" in device_data:
+                device_data = device["data"]
+                new_data = []
+                for d in device_data:
+                    if d["timestamp"]>max_date:
+                        new_data.append(d)
+                devices.update_one({
+                    'mac_address': device["mac_address"]
+                }, {"$set": {
+                    "data": new_data,
+                }
+                }, upsert=False)
         clts = clients.find({})
         for client in clts:
-            client_data = client["data"]
-            new_data = []
-            for d in client_data:
-                if d["timestamp"]>max_date:
-                    new_data.append(d)
-            client.update_one({
-                'mac_address': client["mac_address"]
-            }, {"$set": {
-                "data": new_data,
-            }
-            }, upsert=False)
+            if "data" in client_data:
+                client_data = client["data"]
+                new_data = []
+                for d in client_data:
+                    if d["timestamp"]>max_date:
+                        new_data.append(d)
+                client.update_one({
+                    'mac_address': client["mac_address"]
+                }, {"$set": {
+                    "data": new_data,
+                }
+                }, upsert=False)
