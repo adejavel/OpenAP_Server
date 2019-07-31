e76477e7c54a407cff3f08d88b5daade4e69f412
adejavel <adejavel@cisco.com>
2019-07-31 15:40:34 +0200

    adding file for pass

--- openap_server/settings.py
+++ openap_server/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 from pymongo import MongoClient, errors
+import json
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -149,7 +150,10 @@ USE_TZ = True
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'openap.contact@gmail.com'
-#EMAIL_HOST_PASSWORD = 'O2p0e1n9AP'
+with open('password.json') as json_file:
+    data = json.load(json_file)
+    EMAIL_HOST_PASSWORD = data["email_password"]
+
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
@@ -176,5 +180,3 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     )
 }
-
-this is a tes
